a326b4ee03e1d9df656d8ffe887022b66ed6e96b
BYKI NAGAMANI <bykinagamani22446@gmail.com>
2025-04-07 02:40:44 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3+3) // we are using nodeJS not a browser
+
+console.log(3 + 3)
+
+let name = "javascript"
+let age = 20
+let isLoggedIn = false
+let state
+
+
+//number
+//bigInt
+//boolean =>True / Flase
+//string => ""
+//null => standalone value
+//undefined => not yet assinged a value to it
+//symbol =>unique
+
+
+//object
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
