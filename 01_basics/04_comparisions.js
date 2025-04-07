786c5c9665db70590c848d38187ab535083d1d9a
BYKI NAGAMANI <bykinagamani22446@gmail.com>
2025-04-07 07:19:07 +0000

    comparisions

--- 01_basics/04_comparisions.js
+++ 01_basics/04_comparisions.js
@@ -5,5 +5,22 @@ console.log(2 == 1)
 console.log(2 != 1)
 
 
-console.log("2" > 1)
+console.log("2" > 1) //js automatically converts string into num and then compares
 console.log("02" > 1)
+
+console.log(null < 0)  //false
+console.log(null == 0)  //false
+console.log(null >= 0) //true
+
+/*
+The reason is that an equality check == and comaprisions > >= < <= work differently
+comparisions convert null to a num , treating it as 0
+*/ 
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+
+//===
+
+console.log("2" === 2)
